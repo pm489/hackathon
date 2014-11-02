9eb3ef88fbc285ba5658ea297befab1a53ad69c8
pm489 <pj.meredith@gmail.com>
2014-11-02 21:15:27 +0000

    some new stuff for home side menu
    messing about with side bar stuff

--- Web/www/javascript/app.js
+++ Web/www/javascript/app.js
@@ -4,7 +4,6 @@ var app = angular.module('myApp',['ngCookies']);
 app.controller('mainController',['$scope','$cookies','$cookieStore',
 function(sc,cookies,cookieStore){
 
-
     sc.loggedIn = function(){
       var loggedIn =  cookies.loggedIn;
       if(loggedIn){
@@ -17,6 +16,17 @@ function(sc,cookies,cookieStore){
     sc.logIn = function(){
         cookies.loggedIn=true;
     }
+}]);
+
+app.controller('menuController',['$scope', function(innerSc){
+    innerSc.isActive = function(path){
+        if ($location.path().substr(0, path.length) == path) {
+          return true;
+        } else {
+          return false;
+        }
+
+    }
 }])
 
 app.directive('navBar', function(){
@@ -25,3 +35,9 @@ app.directive('navBar', function(){
         templateUrl: '../html/nav-bar.html'
       }
     });
+app.directive('sideMenu',function(){
+    return{
+        replace: 'true',
+        templateUrl: '../html/side-menu.html'
+    }
+})
