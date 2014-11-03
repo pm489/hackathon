ebadca1e06264e43a44a08680d47d3cf9effd672
pm489 <pj.meredith@gmail.com>
2014-11-03 20:57:47 +0000

    adding angular switch on nav bar
    made it so that the nav bar changes if logged in or not

--- Web/out/production/Web/javascript/app.js
+++ Web/out/production/Web/javascript/app.js
@@ -1,7 +1,32 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp',['ngCookies']);
 
-app.controller('mainController',['$scope',function(){
-    $scope.loggedIn=false;
+
+app.controller('mainController',['$scope','$cookies','$cookieStore',
+function(sc,cookies,cookieStore){
+
+    sc.loggedIn = function(){
+      var loggedIn =  cookies.loggedIn;
+      if(loggedIn){
+            return true;
+      }
+
+        return false;
+    }
+
+    sc.logIn = function(){
+        cookies.loggedIn=true;
+    }
+}]);
+
+app.controller('menuController',['$scope', function(innerSc){
+    innerSc.isActive = function(path){
+        if ($location.path().substr(0, path.length) == path) {
+          return true;
+        } else {
+          return false;
+        }
+
+    }
 }])
 
 app.directive('navBar', function(){
@@ -10,3 +35,9 @@ app.directive('navBar', function(){
         templateUrl: '../html/nav-bar.html'
       }
     });
+app.directive('sideMenu',function(){
+    return{
+        replace: 'true',
+        templateUrl: '../html/side-menu.html'
+    }
+})
