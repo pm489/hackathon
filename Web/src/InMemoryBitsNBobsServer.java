e125c584ce380aa2ecd2ad7782442e36a89ec2d8
Mark Slater <mark.slater@mail.com>
2014-11-13 22:48:48 +0000

    Pending transactions get added to the block chain every 30 seconds.

--- Web/src/InMemoryBitsNBobsServer.java
+++ Web/src/InMemoryBitsNBobsServer.java
@@ -1,19 +1,45 @@
 import client.Trades;
-import ledger.Address;
-import ledger.BlockChain;
-import ledger.Transaction;
+import ledger.*;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public final class InMemoryBitsNBobsServer implements BitsNBobsServer {
 
-    private final BlockChain blockChain = new BlockChain();
+    private final ScheduledExecutorService executorService;
+    private final Object lock = new Object();
+    private BlockChain blockChain = new BlockChain();
+    private List<Transaction> pendingTransactions = new LinkedList<>();
+
+    public InMemoryBitsNBobsServer() {
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lock) {
+                    blockChain = blockChain.addBlock(new Block(pendingTransactions, BlockHash.blockHash(UUID.randomUUID().toString())));
+                    pendingTransactions = new LinkedList<>();
+                }
+            }
+        }, 30, 30, SECONDS);
+    }
 
     @Override
     public Trades get(Address address) {
-        return blockChain.transactionsFor(address).asTrades();
+        synchronized (lock) {
+            return blockChain.transactionsFor(address).asTrades();
+        }
     }
 
     @Override
     public void submit(Transaction transaction) throws InsufficientFundsException {
-        throw new UnsupportedOperationException("Mark didn't implement yet");
+        synchronized (lock) {
+            pendingTransactions.add(transaction);
+        }
     }
 }
