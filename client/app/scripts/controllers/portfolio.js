fcb077f5a03bf13637b09136bd4ba4f7c08777ee
Cash Sun <mr.cashsun@gmail.com>
2014-11-13 13:32:41 +0000

    my profile page.

--- client/app/scripts/controllers/portfolio.js
+++ client/app/scripts/controllers/portfolio.js
@@ -20,13 +20,22 @@ angular.module('clientApp')
 
         $scope.myStocks = [
 
-            {name: 'Google', ticker: 'GOOGL', spot: 547.31, boughtAt:550.02, quantity:100000, status:'Executed'},
-            {name: 'Apple', ticker: 'AAPL',spot:111.25, boughtAt:100.10, quantity:2500, status:'Executed'},
-            {name: 'Credit Suisse', ticker: 'CS', spot:18.20, boughtAt:17.05, quantity:2000000, status:'Executed'},
+            {name: 'Google', ticker: 'GOOGL', spot: 547.31, boughtAt: 550.02, quantity: 100000, status: 'Executed'},
+            {name: 'Apple', ticker: 'AAPL', spot: 111.25, boughtAt: 100.10, quantity: 2500, status: 'Executed'},
+            {name: 'Credit Suisse', ticker: 'CS', spot: 18.20, boughtAt: 17.05, quantity: 2000000, status: 'Executed'},
 
 
-            {name: 'Oracle Corporation', ticker: 'ORCL', spot:20.76, boughtAt:20.80, quantity:7800, status:'Pending Peer Match'},
-            {name: 'Electronic Arts Inc.', ticker: 'EA', spot:219, boughtAt:220, quantity:6550, status:'Pending Sign'}
+            {name: 'Oracle Corporation', ticker: 'ORCL', spot: 20.76, boughtAt: 20.80, quantity: 7800, status: 'Pending Peer Match'},
+            {name: 'Electronic Arts Inc.', ticker: 'EA', spot: 219, boughtAt: 220, quantity: 6550, status: 'Pending Sign'}
+        ];
+
+
+        //{name: 'UbiSoft Entertainment', ticker: 'UBI'},
+//        {name: 'Alibaba Group', ticker: 'BABA'},
+//        {name: 'Oracle Corporation', ticker: 'ORCL'},
+        $scope.myCommissions = [
+            {tradeId: '2132s13dad312dsE', name: 'UbiSoft Entertainment', quantity: 2000, commission: 20, date: '09:20:23.234 13-Nov-2014', status:'done'},
+            {tradeId: '2132s13dad312dsE', name: 'UbiSoft Entertainment', quantity: 2000, commission: 20, date: '09:20:23.234 13-Nov-2014', status: ''}
         ];
 
     });
